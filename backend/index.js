482df2e3468c6517c4b103683135dc4a3f0119c6
Andrei Joldea <andrei.joldea@student.upt.ro>
2024-05-11 17:58:39 +0300

    connection to mongo established

--- backend/index.js
+++ backend/index.js
@@ -2,13 +2,18 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const app = express();
-
-// mongoose
-//     .connect(secret.mongoDbUrl, { useNewUrlParser: true, useUnifiedTopology: true, })
-//     .then(() => { console.log("Connected to MongoDB"); })
-//     .catch((err) => { console.log("Error Connecting to MongoDB"); });
-
 const serverPort = 3000;
+require('dotenv').config();
+
+
+mongoose.connect(process.env.MONGO_CONNECTION_STRING)
+.then(() => { 
+    console.log("Connected to MongoDB"); 
+})
+.catch((err) => { 
+    console.log("Error Connecting to MongoDB"); 
+});
+
 app.listen(serverPort, () => {
     console.log(`Server is running on port: ${serverPort}`);
 });
